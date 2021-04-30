a9d67530a4d423716d33c456de1fb11db9963f32
Ahmed Elnasseh <elnassehag@vcu.edu>
2021-04-29 22:16:17 -0400

    first comm

---
+++ src/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    PORT: process.env.PORT || 8000,
+    NODE_ENV: process.env.NODE_ENV || 'development',
+    DB_URL: process.env.NODE_ENV || 'postgresql://dunder_mifflin@localhost/pick-me-up'
+  }
---
+++ src/server.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const app = require('./app')
+const { DB_URL,PORT } = require('./config')
+const knex = require('knex')
+
+const db = knex({
+  client: 'pg',
+  connection: DB_URL
+})
+
+app.set('db', db)
+
+app.listen(PORT, () => {
+  console.log(`Server listening at http://localhost:${PORT}`)
+})
